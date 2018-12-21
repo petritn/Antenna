64427d197440b93fc3c96486d44bf7fc38abb3bc
petritn <petrit.nahi@gmail.com>
2018-12-21 21:12:28 +0100

    Merge branch 'master' of https://github.com/petritn/Antenna
    Signed-off-by: petritn <petrit.nahi@gmail.com>
    
    # Conflicts:
    #	.idea/Antenna.iml
    #	.idea/misc.xml

--- CArray.py
+++ CArray.py
@@ -1,18 +1,21 @@
 import numpy as np
 from numpy import pi
 
+###############################
+# Linear Array Class Definition
+
 
 class LinearArray(object):
     """Linear Antenna Array
 
     Parameters
     ------------
-    N : int
+    n : int
         Number of antenna elements in the antenna array
-    dX: float
+    dx: float
         Antenna element spacing in the array.
-    E: complex
-        Complex antenna element excitations
+    e: list
+        Complex antenna element excitations, dimension on n
 
     Attributes
     -----------
@@ -23,7 +26,7 @@ class LinearArray(object):
     """
 
     def __init__(self, n, dx, e=None):
-        if E is None:
+        if e is None:
             self.n = n
             self.dx = dx
         else:
@@ -34,8 +37,7 @@ class LinearArray(object):
     def array_factor(self):
         """ Calculate the array factor of the antenna linear array
 
-        Returns
-        -------
+        :return:
         The calculated array factor of the antenna array, for the linear antenna array object
 
         """
@@ -49,21 +51,19 @@ class LinearArray(object):
     def normalized_array_factor(self):
         """ Calculate the normalized array factor of the linear antenna array
 
-        Returns
-        -------
+        :return:
         Normalized antenna array factor
 
         """
         af = np.abs(self.array_factor())
         af_max = np.max(af)
-        naf = af/af_max
+        naf = af / af_max
         return naf
 
     def normalized_power_pattern(self):
         """ Calculate the normalized power pattern
 
-        Returns
-        -------
+        :return:
         Normalized power pattern of the linear array
 
         """
@@ -74,8 +74,7 @@ class LinearArray(object):
     def gain_db(self):
         """ Calculate the gain of the antenna array in dB
 
-        Returns
-        -------
+        :return:
         Array Antenna Gain in dB.
 
         """
@@ -85,7 +84,119 @@ class LinearArray(object):
         gain = 10 * np.log10(max_power / average_power)
         return gain
 
+###############################
+# Planar Array Class Definition
 
 
+class PlanarArray(object):
+    """Planar Antenna Array
 
+        Parameters
+        ------------
+        m: int
+            Number of antenna elements in the X direction of antenna array
+        n: int
+            Number of antenna elements in the Y direction of the antenna array
+        dx: float
+            Antenna element spacing in the X direction of the array.
+        dy: float
+            Antenna element spacing in the Y direction of the array.
+        e: np.array
+            Complex antenna element excitations, dimension of (m, n)
 
+        Attributes
+        -----------
+        Array Factor : nd-array
+            Array factor of the planar antenna array.
+        Gain DB : float
+            Gain of the antenna array
+        """
+
+    def __init__(self, m, n, dx, dy, e=None):
+        if e is None:
+            self.m = m
+            self.n = n
+            self.dx = dx
+            self.dy = dy
+        else:
+            self.m = m
+            self.n = n
+            self.dx = dx
+            self.dy = dy
+            self.e = e
+
+    def array_factor(self):
+        """ Calculate the array factor of the planar antenna array
+
+        :return:
+
+        The calculated array factor of the antenna array, for the planar antenna array object
+
+        """
+        if self.e is None:
+            af = np.zeros((181, 181))
+            for m in range(self.m):
+                for n in range(self.n):
+                    for theta in range(181):
+                        part_theta = 2 * pi * np.sin((theta - 90) * pi / 180)
+                        for phi in range(181):
+                            part_phi = m * self.dx * np.cos(phi * pi / 180) + n * self.dy * np.sin(phi * pi / 180)
+                            af[theta, phi] += np.real(np.exp(1j * (part_theta * part_phi)))
+            return af
+        else:
+            af = np.zeros((181, 181))
+            for m in range(self.m):
+                for n in range(self.n):
+                    for theta in range(181):
+                        part_theta = 2 * pi * np.sin((theta - 90) * pi / 180)
+                        for phi in range(181):
+                            part_phi = m * self.dx * np.cos(phi * pi / 180) + n * self.dy * np.sin(phi * pi / 180)
+                            af[theta, phi] += np.real(self.e[m][n] * np.exp(1j * (part_theta * part_phi)))
+            return af
+
+    def array_factor2(self):
+        """ Calculate the planar array antenna factor as a product of two linear arrays
+
+        :return:
+
+        Calculated antenna array factor as a product of two linear arrays
+
+        """
+
+        af = np.zeros((181, 181))
+        linear_array_x = np.zeros((181, 181))
+        linear_array_y = np.zeros((181, 181))
+
+        if self.e is None:
+            for m in range(self.m):
+                for theta in range(181):
+                    for phi in range(181):
+                        linear_array_x[theta, phi] += np.real(np.exp(1j * m * 2 * pi * self.dx *
+                                                                     np.sin((theta - 90) * pi / 180) *
+                                                                     np.cos(phi * pi / 180) + pi / 2))
+
+            for n in range(self.n):
+                for theta in range(181):
+                    for phi in range(181):
+                        linear_array_y[theta, phi] += np.real(np.exp(1j * n * 2 * pi * self.dy *
+                                                                     np.sin((theta - 90) * pi / 180) *
+                                                                     np.sin(phi * pi / 180)))
+
+        else:
+            for m in range(self.m):
+                for theta in range(181):
+                    for phi in range(181):
+                        linear_array_x[theta, phi] += np.real(self.e[m, 0] * np.exp(1j * m * 2 * pi * self.dx *
+                                                                                    np.sin((theta - 90) * pi / 180) *
+                                                                                    np.cos(phi * pi / 180) + pi / 2))
+
+            for n in range(self.n):
+                for theta in range(181):
+                    for phi in range(181):
+                        linear_array_y[theta, phi] += np.real(self.e[0, n] * np.exp(1j * n * 2 * pi * self.dy *
+                                                                                    np.sin((theta - 90) * pi / 180) *
+                                                                                    np.sin(phi * pi / 180)))
+
+        for theta in range(181):
+            for phi in range(181):
+                af[theta, phi] = linear_array_x[theta, phi] * linear_array_y[theta, phi]
